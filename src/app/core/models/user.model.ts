e91813718e51ef203b6ab759552599843f0cb93f
Mariam Mounir <mounirsalama130@yahoo.com>
2025-06-06 15:31:52 +0300

    save before angular update

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,34 @@
+export class UserRegisterDto{
+    constructor(
+        public email: string,
+        public firstName: string,
+        public lastName: string,
+        public phone: string,
+        public password: string,
+        public confirmPassword: string,
+        public role: string
+    ){}
+
+     passwordsMatch(): boolean {
+    return this.password === this.confirmPassword;
+  }
+}
+
+export class UserLoginDto {
+    constructor(
+        public email: string,
+        public password: string
+    ){}
+}
+
+export class LogedINUser {
+    constructor(
+        public nameidentifier: string,
+        public emailaddress: string,
+        public givenname: string,
+        public surname: string,
+        public UserType: string,
+        public exp: string
+    ){}
+}
+
