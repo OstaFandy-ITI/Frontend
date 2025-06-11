7d05c255b932dcc0f528ccb9f301678557ffd035
Fatma <fatmaelsheikh958@gmail.com>
2025-06-11 03:46:07 +0300

    application

--- src/app/Component/Admin/Admin.routes.ts
+++ src/app/Component/Admin/Admin.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SideNavComponent} from './Layout/side-nav/side-nav.component';
 import { BookingComponent } from './booking/booking.component';
+import { ApplicationComponent } from './application/application.component';
 
 export const adminRoutes: Routes = [
  {
@@ -8,6 +9,7 @@ export const adminRoutes: Routes = [
     component: SideNavComponent, 
     children: [
       { path: 'booking', component: BookingComponent },
+      {path: 'application', component: ApplicationComponent },
     ]
   }
 
