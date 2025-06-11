8e9e83465f0fd4405d37da67d2b4b74d15e0cf2f
ahmed ehab <ahmedehab3088@gmail.com>
2025-06-11 12:56:39 +0300

    m3

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,3 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { AppComponent } from './app/app.component';
-// import { provideHttpClient } from '@angular/common/http';
-// import { provideRouter } from '@angular/router';
-// import { PaymentsComponent } from './app/Component/Admin/payments/payments.component';
-
-// // bootstrapApplication(AppComponent, {
-// //   providers: [
-// //     provideHttpClient(),
-// //     provideRouter([
-// //       // {
-// //       //   path: 'admin',
-// //       //   children: [
-// //       //     { path: 'payments', component: PaymentsComponent },
-// //       //     { path: '', redirectTo: 'payments', pathMatch: 'full' }
-// //       //   ]
-// //       // },
-// //       { path: '', redirectTo: '/admin', pathMatch: 'full' }
-// //     ])
-// //   ]
-// // }).catch(err => console.error(err));
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
