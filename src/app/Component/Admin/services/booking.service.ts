242a0eb202da50cc226d9dd4967cbecf48a91317
Mariam Mounir <mounirsalama130@yahoo.com>
2025-07-03 22:55:02 +0300

    booking tabel in admin

--- src/app/Component/Admin/services/booking.service.ts
+++ src/app/Component/Admin/services/booking.service.ts
@@ -1,18 +1,55 @@
 import { Injectable } from '@angular/core';
 import { URL } from '../../../core/Shared/URL';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {
+  bookingFilterDto,
+  BookingViewModel,
+} from '../../../core/models/Booking.model';
+import { PaginatedResult } from '../../../core/models/category.models';
+import { BookingStatus } from '../../../core/Shared/Enum';
+import { ResponseDto } from '../../../core/models/Response.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookingService {
-
-   private BaseUrl = `${URL.apiUrl}/Booking`;
-  constructor(private http:HttpClient) { }
+  private BaseUrl = `${URL.apiUrl}/Booking`;
+  constructor(private http: HttpClient) {}
 
   ///api/Booking/GetAllBookings
   GetAllBookings(): Observable<any> {
     return this.http.get(`${this.BaseUrl}/GetAllBookings`);
+  }
+
+  ///api/Booking/paged
+  getPaginated(
+    filter: bookingFilterDto
+  ): Observable<PaginatedResult<BookingViewModel>> {
+    let params = new HttpParams()
+      .set('pageNumber', filter.pageNumber)
+      .set('pageSize', filter.pageSize);
+    if (filter.handymanName && filter.handymanName.trim() !== '') {
+      params = params.set('handymanName', filter.handymanName.trim());
+    }
+    if (filter.status && filter.status.trim() !== '') {
+      params = params.set('status', filter.status.trim());
+    }
+
+    if (filter.isActive !== null && filter.isActive !== undefined) {
+      params = params.set('isActive', filter.isActive.toString());
+    }
+
+    return this.http.get<PaginatedResult<BookingViewModel>>(
+      `${this.BaseUrl}/paged`,
+      { params }
+    );
+  }
+  ///api/Booking/CancelBooking
+  CancelBooking(BookingId: number) {
+    return this.http.patch<ResponseDto<string>>(
+      `${this.BaseUrl}/CancelBooking?bookingId=${BookingId}`,
+      {}
+    );
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
