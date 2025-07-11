0e703590040b25e707739e2165cb7e53443b4e83
Mariam Mounir <mounirsalama130@yahoo.com>
2025-07-11 22:00:30 +0300

    Enhance ux in booking

--- src/app/Component/Customer/home/home.component.ts
+++ src/app/Component/Customer/home/home.component.ts
@@ -5,19 +5,32 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../Layout/navbar/navbar.component';
 import { Router, RouterModule } from '@angular/router';
 import { FooterComponent } from '../Layout/footer/footer.component';
-import { ChatbotComponent } from "../chatbot/chatbot.component";
-import { OrderFeedback, OrderFeedbackFilters, OrderFeedbackResponse } from '../../../core/models/Orderfeedback';
+import { ChatbotComponent } from '../chatbot/chatbot.component';
+import {
+  OrderFeedback,
+  OrderFeedbackFilters,
+  OrderFeedbackResponse,
+} from '../../../core/models/Orderfeedback';
 import { OrderFeedbackService } from '../../Admin/services/order-feedback.service.service';
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, NavbarComponent, RouterModule, FooterComponent, ChatbotComponent],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    RouterModule,
+    FooterComponent,
+    ChatbotComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   categories: Category[] = [];
-  allOrdersFeedback: OrderFeedback[] = [];  
-  
+  allOrdersFeedback: OrderFeedback[] = [];
+  pendingCategoryId: number = 0;
+  confirmModal: any;
 
   constructor(
     private categoryService: CategoryService,
@@ -31,19 +44,53 @@ export class HomeComponent {
       next: (res) => (this.categories = res),
       error: (err) => console.error('Failed to load categories', err),
     });
+    
+    this.confirmModal = new bootstrap.Modal(
+      document.getElementById('confirmChangeModal')
+    );
   }
 
   createRange(number: number): number[] {
     return new Array(number);
   }
-  
-  goToBooking(categoryId: number) {
+
+ goToBooking(categoryId: number) {
+    const oldCategoryId = +localStorage.getItem('currentCategoryId')! || 0;
+
+    if (categoryId !== oldCategoryId) {
+      const hasCachedData =
+        localStorage.getItem('selectedServices') ||
+        localStorage.getItem('bookingData');
+
+      if (hasCachedData) {
+        this.pendingCategoryId = categoryId;
+        this.confirmModal.show();
+      } else {
+        this.navigateToBooking(categoryId);
+      }
+    } else {
+      this.navigateToBooking(categoryId);
+    }
+  }
+
+  proceedCategoryChange() {
+    localStorage.removeItem('selectedServices');
+    localStorage.removeItem('bookingData');
+    localStorage.setItem('currentCategoryId', this.pendingCategoryId.toString());
+
+    this.confirmModal.hide();
+
+    this.navigateToBooking(this.pendingCategoryId);
+  }
+
+  private navigateToBooking(categoryId: number) {
+    localStorage.setItem('currentCategoryId', categoryId.toString());
     this.router.navigate(['/booking'], {
       queryParams: { categoryId },
     });
   }
   //handle chatbot
-   showChat = false;
+  showChat = false;
 
   toggleChat() {
     this.showChat = !this.showChat;
@@ -53,21 +100,20 @@ export class HomeComponent {
     this.showChat = false;
   }
 
-   loadOrdersFeedback(): void {
-      
-      const filters: OrderFeedbackFilters = {
-        searchString:'',
-        pageNumber: 1,
-        pageSize: 5
-      };
-  
-      this.orderFeedbackService.getAllOrdersFeedback(filters).subscribe({
-        next: (response: OrderFeedbackResponse) => {
-          this.allOrdersFeedback = response.data;
-        },
-        error: (error) => {
-          console.error('Error loading orders feedback:', error);
-        }
-      });
-    }
+  loadOrdersFeedback(): void {
+    const filters: OrderFeedbackFilters = {
+      searchString: '',
+      pageNumber: 1,
+      pageSize: 5,
+    };
+
+    this.orderFeedbackService.getAllOrdersFeedback(filters).subscribe({
+      next: (response: OrderFeedbackResponse) => {
+        this.allOrdersFeedback = response.data;
+      },
+      error: (error) => {
+        console.error('Error loading orders feedback:', error);
+      },
+    });
+  }
 }
