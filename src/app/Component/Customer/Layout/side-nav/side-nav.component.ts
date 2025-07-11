5c7a6950d2fb124d0ea2db01859c8565a36a9212
Mariam Mounir <mounirsalama130@yahoo.com>
2025-07-11 16:05:46 +0300

    Revert "Merge pull request #60 from OstaFandy-ITI/handyman-profile"
    This reverts commit 95f94831e057e55245d4c606550e7b365e59abbf, reversing
    changes made to b3f2765dfdabdc88a6a536ca663dd4f22d3ee12f.

--- src/app/Component/Customer/Layout/side-nav/side-nav.component.ts
+++ src/app/Component/Customer/Layout/side-nav/side-nav.component.ts
@@ -1,30 +1,23 @@
-import { Component, ViewChild, viewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from "../../../../core/services/auth.service";
-import { NavbarComponent } from '../navbar/navbar.component';
+import { NavbarComponent } from "../navbar/navbar.component"; 
 
 @Component({
   selector: 'app-side-nav',
   standalone: true, 
-  imports: [RouterModule, CommonModule, NavbarComponent], 
+  imports: [RouterModule, CommonModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
-export class SideNavComponent  {
+export class SideNavComponent {
   sidebarOpen = true;
   currentClientId: number | null = null;
-  @ViewChild('sidebarToggle') sidebarToggle: any;
-  
   constructor(private router: Router, private authService: AuthService) { }
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
-    if (!this.sidebarOpen) {
-      this.sidebarToggle.nativeElement.classList.add('toogle-btn-positon');
-    } else {
-      this.sidebarToggle.nativeElement.classList.remove('toogle-btn-positon');
-    }
   }
 
   logout() {
@@ -32,5 +25,3 @@ export class SideNavComponent  {
     this.router.navigate(['/login']);
   }
 }
-
-
